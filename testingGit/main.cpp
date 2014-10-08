f92020b0d168c1ca4ea6def7c16edbdd87bdf0e3
Ibrahim Urooj Saeed <ibrahim.urooj@gmail.com>
2014-10-08 15:21:05 -0400

    Initial Commit

---
+++ testingGit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testingGit
+//
+//  Created by Ibrahim Saeed on 10/8/14.
+//  Copyright (c) 2014 Ibrahim Saeed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
