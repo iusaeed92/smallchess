83ddb0e38488fc287eafd0ee7e1478443bd8e0fd
Adam25T <ajturner@owu.edu>
2014-11-02 15:38:40 -0500

    Skeleton of subclass .cpp
    generatePossibleMoves definition has yet to be written

--- testingGit/Piece.cpp
+++ testingGit/Piece.cpp
@@ -44,3 +44,67 @@ pair<int, int> piece::getPieceCoodinates(){
     return pieceCordinates;
     
 }
+
+
+////////////////////////////////////////////
+//SUBCLASSES
+
+//Pawn
+
+vector<pair<unsigned,unsigned>>  Pawn::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+//Bishop	
+	
+vector<pair<unsigned,unsigned>>  Bishop::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+	
+	
+//Knight
+
+vector<pair<unsigned,unsigned>>  Knight::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+	
+//Castle
+
+vector<pair<unsigned,unsigned>>  Castle::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+	
+//Queen
+
+vector<pair<unsigned,unsigned>>  Queen::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+	
+//King
+
+vector<pair<unsigned,unsigned>>  King::generatePossibleMoves (pair <unsigned, unsigned> pieceCoordinates, gameState currentBoard){
+	
+	//definition required
+
+	}
+
+
+
+
